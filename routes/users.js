1f2fdf6e964a6e8d5d2b6d336111e253fb55572c
Bhavya Pratap Singh <bhavya13701@gmail.com>
2022-07-20 01:01:34 +0530

    First commit

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import {  getUsers, createUser, getUserID, deleteUser, updateUser } from '../controllers/users.js';
+
+const router = express.Router();
+
+
+// all routes starting with /users
+router.get('/', getUsers);
+
+router.post('/',createUser);
+
+router.get('/:id',getUserID);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
+
+export default router;
